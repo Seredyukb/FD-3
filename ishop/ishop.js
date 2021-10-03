76ce01a065cb42c07329bcf626de3cde2de94cfe
Seredyukb <seredyuk.b@gmail.com>
2021-10-03 18:54:24 +0300

    make beautify

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -1,24 +1,44 @@
 var ishop = React.createClass({
 
-    displayName: 'shop',
-  
-    render: function () {
-  
-      var catalogCode = this.props.shopArr.map(v =>
-        React.DOM.tbody({ key: v.code, className: 'block' },
-          React.DOM.tr({ className: 'tr' },
-            React.DOM.td({ className: 'Count', },v.block),
-            React.DOM.img({ className: 'Count', src: v.url, width: 100, height: 100, }),
-            React.DOM.td({ className: 'Count' }, 'Цена:' + v.cost),
-            React.DOM.td({ className: 'Count' }, 'Количество: ' + v.kolvo, ),
-          ),
-        )
-      );
-      return React.DOM.div({ className: 'VotesBlock' },
-        React.DOM.div({ className: 'Question' }, this.props.Maintext),
-        React.DOM.table({ className: 'table' }, catalogCode),
-      );
-    },
-  
-  });
+  displayName: 'shop',
 
+  render: function () {
+
+    var catalogCode = this.props.shopArr.map(v =>
+      React.DOM.tbody({
+          key: v.code,
+          className: 'block'
+        },
+        React.DOM.tr({
+            className: 'tr'
+          },
+          React.DOM.td({
+            className: 'Count',
+          }, v.block),
+          React.DOM.img({
+            className: 'Count',
+            src: v.url,
+            width: 100,
+            height: 100,
+          }),
+          React.DOM.td({
+            className: 'Count'
+          }, 'Цена:' + v.cost),
+          React.DOM.td({
+            className: 'Count'
+          }, 'Количество: ' + v.kolvo, ),
+        ),
+      )
+    );
+    return React.DOM.div({
+        className: 'VotesBlock'
+      },
+      React.DOM.div({
+        className: 'Question'
+      }, this.props.Maintext),
+      React.DOM.table({
+        className: 'table'
+      }, catalogCode),
+    );
+  },
+});
